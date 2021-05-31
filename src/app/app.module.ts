75cb7d4a0a4578f52c4f81e7b293fbac4ae8b65a
leegise-lab <55785075+leegise-lab@users.noreply.github.com>
2021-05-31 13:35:43 +0900

    gise-portfolio 기초 구조생성

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { TestComponent } from './test/test.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent},
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    HomeComponent,
+    TestComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot(routes)
   ],
+  exports: [RouterModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [HomeComponent]
 })
 export class AppModule { }
